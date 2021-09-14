87c25eefa92c778f41f513df6bf682d72d0d3bc0
Allen Li <ali@goodcatch.cn>
2021-09-14 17:30:14 +0800

    重构前端

--- src/Providers/ResourcesServiceProvider.php
+++ src/Providers/ResourcesServiceProvider.php
@@ -33,7 +33,7 @@ class ResourcesServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole ()) {
             $src = __DIR__ . '/../..';
             $this->publishes ([
-                $src . '/resources/js' =>  resource_path('js'),
+                $src . '/public/dist' =>  public_path('core')
             ], 'laravel-modules');
         }
     }
