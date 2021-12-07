6e02c184c565d1f7cf613f179b933ccd9e9e2550
allen <lw@huangyebl.com>
2021-12-07 13:39:37 +0800

    修复路由命名空间

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
  */
 Route::group(
     [
-        'as' => 'man::',
+        'as' => 'core::',
     ],
     function () {
         Route::prefix('Admin')->group(function(){
